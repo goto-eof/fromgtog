2dc1a777f1014cc7d34f87d175338b76db75dc50
Andrei Dodu <andrei.dodu@hotmail.it>
2025-05-10 22:33:11 +0200

    refactor: refactor and about tab

---
+++ src/main/java/com/andreidodu/fromgtog/gui/controller/impl/AboutController.java
@@ -0,0 +1,81 @@
+package com.andreidodu.fromgtog.gui.controller.impl;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.swing.*;
+import java.io.IOException;
+
+@Getter
+@Setter
+public class AboutController {
+
+    public static final String URL_BUY_ME_COFFEE = "https://buymeacoffee.com/andrei.dodu";
+    public static final String URL_PROJECT_WEBSITE = "https://github.com/goto-eof/fromgtog";
+    public static final String URL_REPORT_ISSUE = "https://github.com/goto-eof/fromgtog/issues";
+
+    Logger log = LoggerFactory.getLogger(AboutController.class);
+    private JButton projectWebsiteButton;
+    private JButton reportAnIssueButton;
+    private JButton buyMeACoffeeButton;
+
+    public AboutController(JButton projectWebsiteButton,
+                           JButton reportAnIssueButton,
+                           JButton buyMeACoffeeButton) {
+        this.projectWebsiteButton = projectWebsiteButton;
+        this.reportAnIssueButton = reportAnIssueButton;
+        this.buyMeACoffeeButton = buyMeACoffeeButton;
+        addProjectWebsiteButtonListener();
+        addReportAnIssueButtonListener();
+        addBuyMeACoffeeButtonListener();
+    }
+
+
+    public void addProjectWebsiteButtonListener() {
+        this.projectWebsiteButton.addActionListener(e -> {
+            tryToOpenUrlCrossPlatform(URL_PROJECT_WEBSITE);
+        });
+    }
+
+    private void tryToOpenUrlCrossPlatform(String url) {
+        try {
+            openBrowser("xdg-open", url);
+        } catch (Exception ex) {
+            log.error(ex.getMessage());
+            try {
+                openBrowser("explorer", url);
+            } catch (Exception ex1) {
+                log.error(ex1.getMessage());
+            }
+        }
+    }
+
+    public void addReportAnIssueButtonListener() {
+        this.reportAnIssueButton.addActionListener(e -> {
+            tryToOpenUrlCrossPlatform(URL_REPORT_ISSUE);
+        });
+    }
+
+
+    public void addBuyMeACoffeeButtonListener() {
+        this.buyMeACoffeeButton.addActionListener(e -> {
+            tryToOpenUrlCrossPlatform(URL_BUY_ME_COFFEE);
+
+        });
+    }
+
+    private void openBrowser(String command, String url) {
+        try {
+            ProcessBuilder pb = new ProcessBuilder(command, url);
+            pb.inheritIO();
+            pb.start();
+        } catch (IOException ee) {
+            log.error("failed to open link: {}", ee.getMessage());
+            throw new RuntimeException("failed to open link: " + ee.getMessage());
+        }
+    }
+
+}
