f42f6711782bcb49a475ff33bf6fb14e2f1205cf
Andrei Dodu <andrei.dodu@hotmail.it>
2025-05-11 16:59:09 +0200

    refactor

--- src/main/java/com/andreidodu/fromgtog/service/factory/to/engines/strategies/generic/GenericDestinationEngineFromStrategyService.java
+++ src/main/java/com/andreidodu/fromgtog/service/factory/to/engines/strategies/generic/GenericDestinationEngineFromStrategyService.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public interface GenericDestinationEngineFromStrategyService {
     String getLogin(String token, String urlString);
 
-    boolean createRepository(String url, String token, String repositoryName, String s, boolean equals) throws IOException, InterruptedException;
+    boolean createRepository(String url, String token, String repositoryName, String description, boolean isPrivate) throws IOException, InterruptedException;
 
     boolean updateRepositoryPrivacy(String token, String login, String url, String repositoryName, boolean isArchived, boolean isPrivate) throws IOException, InterruptedException;
 }
