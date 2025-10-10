381350384bb4c0ae6462625fb53734595e4faea4
Andrei Dodu <andrei.dodu@hotmail.it>
2025-10-10 15:14:01 +0200

    feat: validator (composite + factory pattern in order to make the code more clear)

--- src/main/java/com/andreidodu/fromgtog/gui/util/GuiUtil.java
+++ src/main/java/com/andreidodu/fromgtog/gui/util/GuiUtil.java
@@ -10,7 +10,6 @@ public class GuiUtil {
 
     final static Logger log = LoggerFactory.getLogger(GuiUtil.class);
 
-
     public static void addActionListenerToChooseReposListFileButton(final JButton button, final JTextField textField) {
         button.addActionListener(e -> {
             JFileChooser chooser = new JFileChooser();
