472b132f708f2024d92f4a710e92a659e08dcfc1
Andrei Dodu <andrei.dodu@hotmail.it>
2025-10-22 11:42:32 +0200

    fix: snap yml

--- snap/local/prime.sh
+++ snap/local/prime.sh
@@ -4,11 +4,11 @@ set -eux
 
 LIB_DIR=$(find "$SNAPCRAFT_PRIME/usr/lib/" -maxdepth 1 -type d -name "*-linux-gnu" -print -quit)
 
-if [ -d "$LIB_DIR" ]; then
+if [ -f "$LIB_DIR/libappindicator3.so.1.0.0" ]; then
   (cd "$LIB_DIR" && {
       ln -sf libappindicator3.so libappindicator3.so.1.0.0
+#      ln -sf libasound.so libasound.so.2.0.0
 #      ln -sf libasound.so liboss4-salsa.so.2.0.0
-      ln -sf libasound.so libasound.so.2.0.0
 #      ls -sf libpixbufloader-svg.so librsvg-2.so.2
   })
 fi
