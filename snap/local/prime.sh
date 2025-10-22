936ee584ff1eaf133305e67f69e0fed9cdd4ea81
Andrei Dodu <andrei.dodu@hotmail.it>
2025-10-23 01:14:59 +0200

    tst

--- snap/local/prime.sh
+++ snap/local/prime.sh
@@ -14,32 +14,32 @@ return 0
 ##########################################
 # workaround
 ##########################################
-
-copy_lib_with_new_name() {
-    local source_path="$1"
-    local destination_path="$2"
-    local source_lib_pattern="$3"
-    local destination_lib_name="$4"
-
-    mkdir -p "$destination_path"
-
-    lib=$(find "$source_path" -type f -name "$source_lib_pattern" | head -n1)
-
-    if [ -f "$lib" ]; then
-        echo "Copying library:"
-        echo "  SOURCE:      $lib"
-        echo "  Destination: $destination_path/$destination_lib_name"
-        cp -L "$lib" "$destination_path/$destination_lib_name"
-    else
-        echo "No library matching $source_lib_pattern found in $source_path."
-    fi
-}
-
-SOURCE="/root"
-DESTINATION="$SNAPCRAFT_PRIME/usr/lib/$CRAFT_ARCH_TRIPLET_BUILD_FOR"
-
-mkdir -p "$DESTINATION"
-
-copy_lib_with_new_name "$SOURCE" "$DESTINATION" "libappindicator3.so*" "libappindicator3.so"
-copy_lib_with_new_name "$SOURCE" "$DESTINATION" "liboss4-salsa.so.2.0.*" "libasound.so"
-copy_lib_with_new_name "$SOURCE/alsa-lib" "$DESTINATION" "libasound_module_pcm_pulse.so" "libasound_module_pcm_pulse.so"
+#
+#copy_lib_with_new_name() {
+#    local source_path="$1"
+#    local destination_path="$2"
+#    local source_lib_pattern="$3"
+#    local destination_lib_name="$4"
+#
+#    mkdir -p "$destination_path"
+#
+#    lib=$(find "$source_path" -type f -name "$source_lib_pattern" | head -n1)
+#
+#    if [ -f "$lib" ]; then
+#        echo "Copying library:"
+#        echo "  SOURCE:      $lib"
+#        echo "  Destination: $destination_path/$destination_lib_name"
+#        cp -L "$lib" "$destination_path/$destination_lib_name"
+#    else
+#        echo "No library matching $source_lib_pattern found in $source_path."
+#    fi
+#}
+#
+#SOURCE="/root"
+#DESTINATION="$SNAPCRAFT_PRIME/usr/lib/$CRAFT_ARCH_TRIPLET_BUILD_FOR"
+#
+#mkdir -p "$DESTINATION"
+#
+#copy_lib_with_new_name "$SOURCE" "$DESTINATION" "libappindicator3.so*" "libappindicator3.so"
+#copy_lib_with_new_name "$SOURCE" "$DESTINATION" "liboss4-salsa.so.2.0.*" "libasound.so"
+#copy_lib_with_new_name "$SOURCE/alsa-lib" "$DESTINATION" "libasound_module_pcm_pulse.so" "libasound_module_pcm_pulse.so"
