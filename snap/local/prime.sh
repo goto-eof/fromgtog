ac303b38bb2039745aafdb9cc0b42e5ef2d1c041
Andrei Dodu <andrei.dodu@hotmail.it>
2025-10-22 15:57:26 +0200

    fix: snap yml + change versiondo you remember the service name

--- snap/local/prime.sh
+++ snap/local/prime.sh
@@ -2,16 +2,16 @@
 
 set -eux
 
-SOURCE_DIR="$SNAPCRAFT_STAGE/usr/lib/$CRAFT_ARCH_TRIPLET_BUILD_FOR"
+SOURCE_DIR="gnome-platform/usr/lib/$CRAFT_ARCH_TRIPLET_BUILD_FOR"
 DESTINATION_DIR="$SNAPCRAFT_PRIME/usr/lib/$CRAFT_ARCH_TRIPLET_BUILD_FOR"
 
 mkdir -p "$DESTINATION_DIR"
+# WORKAROUND
 # I am looking for libappindicator3.so in the destination dir, if it does not exists then copy.
-# this because snapcraft skips some libraries, including the one that I want libappindicator3.so
+# this because snapcraft skips some libraries, including the one that I need, libappindicator3.so,
+# otherwise the system tray not works and the application does not start
 for lib in "$SOURCE_DIR"/libappindicator3.so*; do
-  BASENAME="$(basename "$lib")"
-  SHORTNAME="${BASENAME%%.so*}.so"
-  if [ ! -f "$DESTINATION_DIR/$SHORTNAME" ]; then
-    cp -L "$lib" "$DESTINATION_DIR/$SHORTNAME"
+  if [ ! -f "$DESTINATION_DIR/libappindicator3.so" ]; then
+    cp -L "$lib" "$DESTINATION_DIR/libappindicator3.so"
   fi
 done
